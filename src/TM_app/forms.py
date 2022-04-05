d16cd1d155f16e2b798376c6a9078e68320a5499
Aleksey170999 <alexbooster3@yandex.ru>
2022-04-05 10:59:37 +0300

    first commit

---
+++ src/TM_app/forms.py
@@ -0,0 +1,25 @@
+from django.forms import ModelForm, DateTimeInput, DateTimeInput, TextInput
+from .models import TimesModel
+
+
+class TimesForm(ModelForm):
+    class Meta:
+        model = TimesModel
+        fields = ['user_name', 'in_time', 'out_time']
+
+        widgets = {
+            "in_time": DateTimeInput(format="%d %b %Y %H:%M:%S",
+                                     attrs={'class': 'form-control',
+                                            'placeholder': 'Пришёл'
+                                            }),
+            "out_time": DateTimeInput(format="%d %b %Y %H:%M:%S",
+                                      attrs={
+                                          'class': 'form-control',
+                                          'placeholder': 'Ушёл'
+
+                                      }),
+            "user_name": TextInput(attrs={
+                                          'class': 'form-control',
+                                          'placeholder': 'Ушёл'
+                                    }),
+        }
---
+++ src/TM_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .forms import TimesForm
+from .models import TimesModel
+
+# def times_list(request):
+#     data = TimesForm.objects.all()
+#     return render(request, 'TM_app/times.html', data)
+
+
+def create(request):
+    if request.method == "POST":
+        form = TimesForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = TimesForm()
+
+    form = TimesForm()
+
+    all_times = []
+    times = TimesModel.objects.all()
+    for time in times:
+        time_info = {
+            'user_name': time.user_name,
+            'in_time': time.in_time,
+            'out_time': time.out_time
+            }
+        all_times.append(time_info)
+        
+    data = {
+        'form': form,
+        'times': all_times
+    }
+
+    return render(request, 'TM_app/times.html', data)
